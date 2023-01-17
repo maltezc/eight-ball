6c700ec49b3444f2ddd08fbad87b126c325c1987
Jesse Gelburd-Meyers <meyburdj@gmail.com>
2023-01-17 12:45:13 -0800

    finished

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,23 +1,35 @@
 import './App.css';
-import answers from './answers';
+// import answers from './Answers';
 import { useState } from 'react';
 
 import React from 'react'
 
- function EightBall({answers=answers}) {
 
-  const [eightBall, setEightBall] = useState({message: "Think of a Question", color: "black"})
+
+function EightBall({ answers }) {
+  console.log("answers:", answers)
+  const [eightBall, setEightBall] = useState({ msg: "Think of a Question", color: "black" })
+  console.log(eightBall)
 
   function changeBall(evt) {
     const randomIndex = Math.floor(Math.random() * answers.length);
-    const answer = answers[randomIndex];
-
-    setEightBall({answer.message, });
+    const eightBall = answers[randomIndex];
+    setEightBall(eightBall);
   }
 
+  let circle = {
+    "background-color": eightBall.color,
+    margin: "50px",
+    height: "200px",
+    width: "200px",
+    "border-radius": "50 %",
+    display: "inline - block"
+  }
+
+
   return (
-    <div>
-      {answers};
+    <div className="circle" style={circle} onClick={changeBall}>
+      {eightBall.msg}
 
     </div>
   )
